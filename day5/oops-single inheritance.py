02f7345a96d41835afe369fdb50227a7cff9c4c2
Sangamithra <sangamithra0714@gmail.com>
2026-06-24 22:49:23 +0530

    Implement single inheritance with person and student classes

---
+++ day5/oops-single inheritance.py
@@ -0,0 +1,9 @@
+class person:
+    def habbit(self):
+        print("playing games in ps5")
+class student(person):
+    def studies(self):
+        print("learns python daily")
+s=student()
+s.studies()
+s.habbit()
