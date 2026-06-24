3cda317ec1b412589837d26153a38a4d6fe899c4
Sangamithra <sangamithra0714@gmail.com>
2026-06-24 21:51:16 +0530

    Create largest 3 num.py

---
+++ day1/largest 3 num.py
@@ -0,0 +1,13 @@
+a=4
+b=4
+c=4
+if a>b and a>c:
+   print("a is large")
+elif b>a and b>c:
+    print("b is large")
+elif c>a and c>b:
+    print("c is large")
+elif a==b and b==c:
+    print("All numbers are equal")
+else:
+    print("Invalid input")
